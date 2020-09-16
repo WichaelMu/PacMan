19acf240a67dcc68669bcc4bbbfa4b70b1a7dd68
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2020-09-16 13:56:43 +1000

    PacMan

---
+++ Assets/Scripts/PacManController.cs
@@ -0,0 +1,238 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PacManController : MonoBehaviour
+{
+
+    Rigidbody PacMan;
+    GameObject switcher;
+
+    public float moveSpeed;
+
+    float DefaultMoveSpeed;
+    bool LR = false, IsStill;
+    string nextAvailableTurn, CurrentDirection;
+
+    void Awake()
+    {
+        PacMan = GetComponent<Rigidbody>();
+        DefaultMoveSpeed = moveSpeed;
+
+        ResetGame();
+
+        R();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        KeyboardControl();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Switcher"))
+        {
+            this.switcher = other.gameObject;
+            Switcher switcher = other.GetComponent<Switcher>();
+
+            if (switcher.allowDirection(nextAvailableTurn))
+                Invoke(nextAvailableTurn, 0f);
+
+            if (!switcher.allowDirection(CurrentDirection))
+                HoldPosition(true);
+        }
+
+        if (other.CompareTag("Pellet"))
+            EatPellet(other.gameObject);
+        if (other.CompareTag("BigPellet"))
+            EatBigPellet(other.gameObject);
+    }
+
+    void EatPellet(GameObject Pellet)
+    {
+        Destroy(Pellet);
+        PlayerStats.score += 10;
+    }
+    void EatBigPellet(GameObject BigPellet)
+    {
+        Destroy(BigPellet);
+    }
+
+    void PacManIsDead()
+    {
+        //TODO: Play the death animation for Pac Man.
+    }
+    void ResetGame()
+    {
+        LR = false;
+        PlayerStats.score = 0;
+    }
+
+    #region The Movement of Pac Man
+
+    void Movement()
+    {
+        PacMan.MovePosition(transform.position + (transform.right * moveSpeed * Time.deltaTime));
+    }
+
+    void KeyboardControl()
+    {
+        string keyboardIn = Input.inputString;
+
+        #region WASD Controls
+        switch (keyboardIn)
+        {
+            case "w":
+                determineRotation("U");
+                return;
+            case "W":
+                determineRotation("U");
+                return;
+            case "s":
+                determineRotation("D");
+                return;
+            case "S":
+                determineRotation("D");
+                return;
+            case "a":
+                determineRotation("L");
+                return;
+            case "A":
+                determineRotation("L");
+                return;
+            case "d":
+                determineRotation("R");
+                return;
+            case "D":
+                determineRotation("R");
+                return;
+        }
+
+        #endregion
+
+        #region ARROW KEY Controls
+        if (Input.GetKey(KeyCode.UpArrow))      determineRotation("U");
+        if (Input.GetKey(KeyCode.DownArrow))    determineRotation("D");
+        if (Input.GetKey(KeyCode.LeftArrow))    determineRotation("L");
+        if (Input.GetKey(KeyCode.RightArrow))   determineRotation("R");
+        #endregion
+    }
+
+    void determineRotation(string whereTo)
+    {
+
+        if (IsStill)
+        {
+            Switcher s = switcher.GetComponent<Switcher>(); //  If Pac Man is still, you're probably on a corner.
+
+            if (s.allowDirection(whereTo))
+                Invoke(whereTo, 0f);
+            return;
+        }
+
+        if (LR)
+        {
+            if (whereTo == "L") { L(); return; }
+            if (whereTo == "R") { R(); return; }
+        }
+        else nextTurn(whereTo);
+
+        if (!LR)
+        {
+            if (whereTo == "U") { U(); return; }
+            if (whereTo == "D") { D(); return; }
+        }
+        else nextTurn(whereTo);
+    }
+
+    #region The Rotation of Pac Man
+
+    void U()    //  Up turn.
+    {
+        rotateP(new Vector3(000f, 000f, 090f)); //  Rotates Pac Man to face up.
+
+        //Debug.Log("Moving Up");
+        PlayerStats.timesTurnedU++;
+
+        NullifyNextAvTurn();
+        CurrentDirection = "U";
+
+        HoldPosition(false);
+        LR = false;
+    }
+
+    void D()    //  Down turn.
+    {
+        rotateP(new Vector3(000f, 000f, 270f)); //  Rotates Pac Man to face down.
+
+        //Debug.Log("Moving Down");
+        PlayerStats.timesTurnedD++;
+
+        NullifyNextAvTurn();
+        CurrentDirection = "D";
+
+        HoldPosition(false);
+        LR = false;
+    }
+
+    void L()    //  Left turn.
+    {
+        rotateP(new Vector3(000f, 000f, 180f)); //  Rotates Pac Man to face left.
+
+        //Debug.Log("Moving Left");
+        PlayerStats.timesTurnedL++;
+
+        //nextTurn("L");
+        NullifyNextAvTurn();
+        CurrentDirection = "L";
+
+        HoldPosition(false);
+        LR = true;
+    }
+
+    void R()    //  Right turn.
+    {
+        rotateP(new Vector3(000f, 000f, 000f)); //  Rotates Pac Man to face right.
+
+        //Debug.Log("Moving Right");
+        PlayerStats.timesTurnedR++;
+
+        NullifyNextAvTurn();
+        CurrentDirection = "R";
+
+        HoldPosition(false);
+        LR = true;
+    }
+
+    void rotateP(Vector3 faceAngle)
+    {
+        transform.localEulerAngles = faceAngle;
+    }
+
+    #endregion
+
+    void nextTurn(string whereTo)
+    {
+        nextAvailableTurn = whereTo;
+    }
+    void NullifyNextAvTurn()
+    {
+        nextAvailableTurn = null;
+    }
+
+    void HoldPosition(bool instruction)
+    {
+        if (instruction)
+        {
+            moveSpeed = 0f;
+            IsStill = true;
+            return;
+        }
+        moveSpeed = DefaultMoveSpeed;
+        IsStill = false;
+    }
+
+    #endregion
+
+}
