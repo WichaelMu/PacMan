9fe8e7069cbfb8810ff98fe5ba4b2f114b41b61b
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2020-09-16 13:57:59 +1000

    Player Statistics

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public TextMeshProUGUI CurrentScore;
+
+    public static int timesTurnedU = 0, timesTurnedD = 0, timesTurnedL = 0, timesTurnedR = 0;
+    public static int score = 0;
+    public static int highScore = 0;
+    public static int timesDied = 0;
+    public static float timePlayed = 0f;
+
+    public static void UpdateScore()
+    {
+        //CurrentScore.text = score.ToString();
+    }
+}
