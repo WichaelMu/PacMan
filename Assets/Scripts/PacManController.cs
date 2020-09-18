8006951475f81936b75283fa2e8a65d21a425bf4
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2020-09-18 15:34:44 +1000

    Update PacManController.cs
    PacMan will now stop moving when dead.

--- Assets/Scripts/PacManController.cs
+++ Assets/Scripts/PacManController.cs
@@ -23,7 +23,7 @@ public class PacManController : MonoBehaviour
     public float moveSpeed;
 
     float DefaultMoveSpeed;
-    bool LR = false, IsStill, IsOutOfPortal = true;
+    bool LR = false, IsStill, IsOutOfPortal = true, IsAlive;
     string nextAvailableTurn, CurrentDirection;
 
     void Start()
@@ -118,6 +118,7 @@ public class PacManController : MonoBehaviour
         FindObjectOfType<AudioController>().StopAllSounds();
         PlaySound("PACMANDEATH");
         moveSpeed = 0;
+        IsAlive = false;
 
         Invoke("ResetGame", 4f);
     }
@@ -144,6 +145,7 @@ public class PacManController : MonoBehaviour
 
     void ResetGame()
     {
+        IsAlive = true;
         DeadStateAnim.SetBool("PacManIsDead", false);
         moveSpeed = DefaultMoveSpeed;
         IsOutOfPortal = true;
@@ -326,10 +328,10 @@ public class PacManController : MonoBehaviour
             IsStill = true;
             return;
         }
-        moveSpeed = DefaultMoveSpeed;
+        if (IsAlive)
+            moveSpeed = DefaultMoveSpeed;
         IsStill = false;
     }
 
     #endregion
-
 }
