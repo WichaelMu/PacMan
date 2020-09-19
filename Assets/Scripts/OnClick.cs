1f238713e45718c55fb9297f0c9240078246c810
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2020-09-19 18:36:57 +1000

    Main Menu

--- Assets/Scripts/OnClick.cs
+++ Assets/Scripts/OnClick.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OnClick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
+        Debug.Log("Game Quit");
     }
 }
