e8fcdc890f7bbdb509be99566a245b2b49ab8f90
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2020-10-07 21:14:15 +1100

    Start Scene

--- Assets/Scripts/OnClick.cs
+++ Assets/Scripts/OnClick.cs
@@ -1,8 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class OnClick : MonoBehaviour
 {
+
+    public TextMeshProUGUI CurrentHighScore;
+    public TextMeshProUGUI TimePlayed;
+
+    void Awake()
+    {
+        PlayerStats.LoadGame();
+        try { CurrentHighScore.text = PlayerStats.highScore.ToString(); } catch (System.NullReferenceException){  }
+
+        int milli = PlayerStats.timePlayed;
+        int c = 0;
+        int seconds = 0, minute = 0;
+
+        for (int i = 0; i < milli; i++)
+        {
+            c++;
+            if (c == 100)
+            {
+                seconds++;
+                c = 0;
+            }
+
+            if (seconds == 60)
+            {
+                minute++;
+                seconds = 0;
+            }
+        }
+        TimePlayed.text = minute + ":" + seconds + ":" + milli;
+    }
+
     public void PlayIntermission()
     {
         SceneManager.LoadScene(1);
